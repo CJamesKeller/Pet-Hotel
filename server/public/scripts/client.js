f936ec0ad5388b4c4b242188286809cf8097bbfb
Logan Kelly <logan.r.kelly@gmail.com>
2017-03-24 15:26:16 -0500

    Git merge. Fixed conflicts.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,12 +6,8 @@ $(document).ready(function(){
 });
 
 function addEventListeners() {
-
-  $(".ownerForm").on("mouseover", function()
-{
-  console.log("you moused over!");
-});
-
+  $('.addPet').on('submit', addPetPostRequest);
+  
   $('h1').on('click', function(){
     $('h1').text('Cat Hotel');
   });
@@ -21,21 +17,48 @@ function addEventListeners() {
 
   console.log("Hahaha, conflict!");
 
-}//ends addEventListeners
+  //Start of Logan's add
+  $('#firstNameInput').on('click', function(){
+    console.log('firstNameInput');
+  });
 
+  $('#lastNameInput').on('click', function(){
+    console.log('lastNameInput');
+  });
+
+  $('#addOwnerButton').on('click', function(){
+    console.log('addOwnerButton');
+    $.ajax({
+      type: 'POST',
+      url: '/owners/add',
+      data: {
+        firstName: $(('#firstNameInput')).val(),
+        lastName: $('#lastNameInput').val(),
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+  });
+  // End of Logan's add
+
+}//ends addEventListeners
 
 function addPetPostRequest (event) {
   event.preventDefault();
   var petObject = {};
-  petObject.name = $(this).children('#petFormNameInput').val();
-  petObject.breed = $(this).children('#petFormBreedInput').val();
-  petObject.color = $(this).children('#petFormColorInput').val();
+  //petObject.name = $('#petFormNameInput').val();
+  petObject.breed = $('#petFormBreedInput').val();
+  petObject.color = $('#petFormColorInput').val();
   console.log(petObject);
-  // $.ajax({
-  //   url: '/pets/add',
-  //   type: 'POST',
-  //   data: {param1: 'value1'}
-  // });
+  $.ajax({
+    url: '/pets/add',
+    type: 'POST',
+    data: petObject,
+    success: function (res) {
+      console.log(res);
+    }
+  });
 }
 
 //This will be called using a response from server!!
@@ -51,18 +74,18 @@ function appendDom()
     $el.append("<td>" + petInfo.breed + "</td>");
     $el.append("<td>" + petInfo.color + "</td>");
     $el.append("<td><button class='delete' data-petInfo='" +
-                petInfo.id +
-              "'>Delete</button></td>");
+    petInfo.id +
+    "'>Delete</button></td>");
     $el.append("<td><button class='edit' data-petInfo='" +
-                petInfo.id +
-                "' data-owner='" + petInfo.owner +
-                "' data-name='" + petInfo.name +
-                "' data-breed='" + petInfo.breed +
-                "' data-color='" + petInfo.color +
-              "'>Edit</button></td>");
+    petInfo.id +
+    "' data-owner='" + petInfo.owner +
+    "' data-name='" + petInfo.name +
+    "' data-breed='" + petInfo.breed +
+    "' data-color='" + petInfo.color +
+    "'>Edit</button></td>");
     $el.append("<td><button class='delete' data-petInfo='" +
-                petInfo.id +
-              "'>Delete</button></td>");
+    petInfo.id +
+    "'>Delete</button></td>");
 
   }
 }
@@ -73,9 +96,9 @@ function getData(){
     type: 'GET',
     url: '/pets',
     success: function(response){
-        console.log("I've returned from /pets with this:", response);
-        var dataArray = response;
-        displayData(dataArray);
+      console.log("I've returned from /pets with this:", response);
+      var dataArray = response;
+      displayData(dataArray);
     }
   });//ends ajax get
 }//ends getData
