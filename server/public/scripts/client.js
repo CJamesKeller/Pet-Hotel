183d47d87d94065217b6cd979905d2ed9a2db1b2
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-24 14:30:14 -0500

    saves one change

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,23 +1,38 @@
 $(document).ready(function(){
   console.log('js sourced');
+  getData();
   addEventListeners();
 });
 
 function addEventListeners() {
-  $('.addPet').on('submit', addPetPostRequest);
+
+  $(".ownerForm").on("mouseover", function()
+{
+  console.log("you moused over!");
+});
+  //Event listeners will go here.
+
+  $('h1').on('click', function(){
+    $('h1').text('Cat Hotel');
+  });
+  $("#ownerForm").on("click","button",function(){
+    console.log("ownerForm on click button");
+  });//end ownerForm On Click button
+
+  console.log("Hahaha, conflict!");
+
+
 }
 
-function addPetPostRequest (event) {
-  event.preventDefault();
-  var petObject = {};
-  petObject.name = $(this).children('#petFormNameInput').val();
-  petObject.breed = $(this).children('#petFormBreedInput').val();
-  petObject.color = $(this).children('#petFormColorInput').val();
-  console.log(petObject);
-  // $.ajax({
-  //   url: '/pets/add',
-  //   type: 'POST',
-  //   data: {param1: 'value1'}
-  // });
 
-}
+function getData(){
+  $.ajax({
+    type: 'GET',
+    url: '/pets',
+    success: function(response){
+        console.log("I've returned from /pets with this:", response);
+        var dataArray = response;
+        displayData(dataArray);
+    }
+  });//ends ajax get
+}//ends getData
