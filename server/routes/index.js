100c2a7feb7bb25db1fa0ae904372545b7b8b65f
Logan Kelly <logan.r.kelly@gmail.com>
2017-03-24 14:32:39 -0500

    html and client communicating

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var path = require('path');
 
+router.get('/', function( req, res){
+  console.log('index.js hits base url');
+  res.sendFile(path.resolve('index.html'));
+});
 
 
-module.exports = 
+module.exports = router;
